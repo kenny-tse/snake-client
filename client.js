0ba54344189e358fcb7e436163720b5501635356
kenny-tse <kennytse91@gmail.com>
2021-09-22 20:57:53 +0000

    swapped play and client and added interval of up commands

--- client.js
+++ client.js
@@ -1,4 +1,54 @@
-const { connect } = require("./play");
+const net = require("net");
+const readline = require(`readline`);
 
-connect;
+const rl = readline.createInterface({
+  input: process.stdin
+});
 
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.223.133",
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //logs whatever the server sends to you
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  //write to server your initials
+  conn.on('connect', () => {
+    conn.write('Name: KT');
+  });
+
+  // conn.on('data', (data) => {
+  //   conn.write('Move: up');
+  // });
+
+  rl.on('line', (input) => {
+    conn.write(input);
+  })
+
+  let time = 1000;
+
+  for (let i = 0; i < 50; i++) {
+
+    setInterval(() => {
+
+      conn.write("Move: up");
+      time += 1000;
+
+    }, time);
+
+  }
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,32 +1,7 @@
-const net = require("net");
-const readline = require(`readline`);
+const { connect } = require("./client");
 
-const rl = readline.createInterface({
-  input: process.stdin
-});
+connect;
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133",
-    port: 50542
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
 
-  conn.on('connect', () => {
-    conn.write('Name: KT');
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
-
-module.exports = { connect };
